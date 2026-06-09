743216fb2018eb5ead37022070adee7065ccd45c
aditya <adityashukla9179@gmail.com>
2026-06-09 15:22:02 +0530

    camera switch botten added in admin page

--- server.js
+++ server.js
@@ -59,6 +59,10 @@ io.on('connection', (socket) => {
     io.to(employeeId).emit('stream:requested', { adminId: socket.id });
   });
 
+  socket.on('admin:switch-camera', ({ employeeId }) => {
+    io.to(employeeId).emit('stream:switch-camera');
+  });
+
   socket.on('webrtc:offer',  ({ to, offer })     => io.to(to).emit('webrtc:offer',  { from: socket.id, offer }));
   socket.on('webrtc:answer', ({ to, answer })    => io.to(to).emit('webrtc:answer', { from: socket.id, answer }));
   socket.on('webrtc:ice',    ({ to, candidate }) => io.to(to).emit('webrtc:ice',    { from: socket.id, candidate }));
